f873fe36623b2762d2a19d766d0db17344672ece
vaghamashi <vaghamashir120@gmail.com>
2023-03-27 10:59:43 +0530

    .

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { Button } from 'react-bootstrap';
+import React from 'react';
+import {useNavigate} from "react-router-dom"
 
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickLogin = () => {
+    navigate('/')
+  }
   return (
     <div>
       <header className="header axil-header header-style-1">
@@ -8,7 +14,7 @@ const Header = () => {
           <div className="container">
             <div className="row">
               <div className="logo">
-                <a href="index.html"><img src="image/logo.svg" alt="logo" /></a>
+                <a><img src="image/logo.svg" alt="logo" /></a>
               </div>
               <nav>
                 <ul className="menu">
@@ -44,35 +50,16 @@ const Header = () => {
                   </li>
                   <li className="dropdown">
                     <a href="javascript:void(0)" className="topper subtoper-4">Pages <i className="fa-solid fa-angle-down" />
-                    </a><ul className="submenu submenu-4"><a href="javascript:void(0)" className="topper subtoper-4">
-                    </a><li><a href="javascript:void(0)" className="topper subtoper-4" /><a href="javascript:void(0)">About Us</a></li>
-                      <li><a href="javascript:void(0)">Our Office</a></li>
-                      <li><a href="javascript:void(0)">Case Study</a></li>
-                      <li><a href="javascript:void(0)">Case Study Details</a></li>
-                      <li><a href="pages.html">Team</a></li>
-                      <li><a href="javascript:void(0)">Team Details</a></li>
-                      <li><a href="javascript:void(0)">Our Clients</a></li>
-                      <li><a href="javascript:void(0)">Testimonial</a></li>
-                      <li><a href="javascript:void(0)">Pricing Table</a></li>
-                      <li><a href="javascript:void(0)">Typography</a></li>
-                      <li><a href="javascript:void(0)">404 Page</a></li>
-                      <li><a href="javascript:void(0)">Coming Soon</a></li>
-                    </ul>
+                    </a>
                   </li>
                   <li className="dropdown">
                     <a href="javascript:void(0)" className="topper subtoper-5">Blog <i className="fa-solid fa-angle-down" />
-                    </a><ul className="submenu submenu-5"><a href="javascript:void(0)" className="topper subtoper-5">
-                    </a><li><a href="javascript:void(0)" className="topper subtoper-5" /><a href="blog.html">Blog</a></li>
-                      <li><a href="javascript:void(0)">Standard Post</a></li>
-                      <li><a href="javascript:void(0)">Gallery Post</a></li>
-                      <li><a href="javascript:void(0)">Video Post</a></li>
-                    </ul>
+                    </a>
                   </li>
                   <li><a href="contact.html">Contact</a></li>
                 </ul>
-                <div className="menu-bar">
-                  <a href="javascript:void(0)"><i className="fa-solid fa-bars-staggered" /></a>
-                </div>
+                
+                  <Button variant="primary"type='button ' className='h-25' onClick={onClickLogin}>Login</Button>
               </nav>
             </div>
           </div>
